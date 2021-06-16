c4960248427afaf877d3499ced3ae8c1ace187c8
Manuel[Μ¥ĄĐ] <78708840+ManuelGamer13@users.noreply.github.com>
2021-06-17 05:32:45 +0800

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -1,17 +1,18 @@
 const desmenu = (prefix, pushname) => {
     return `
 *Comandos De Descargas ✅*
+ 💜Manuel Diaz💜
  
-- ${prefix}play
-  Coloca el nombre de la cancion. O el link del video YT
-- ${prefix}ytmp3
-  Coloca el link del video YT
-- ${prefix}ytmp4
-  Coloca el link del video YT
+- ${prefix} play
+  Coloca el nombre de la cancion. O el link del video YouTube
+- ${prefix} ytmp3
+  Coloca el link del video YouTube
+- ${prefix} ytmp4
+  Coloca el link del video YouTube
     
-_*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_
+_*play tiene un limite de 19.500.000 canciones si llega a su limite se restablecera dentro de 24 horas_
      
-by shanduy`
+by  💜Manuel Diaz💜`
 
 }
 
